362d181ec8acb02aae80747a0c06b071b0bab84a
mint <mint924@gmail.com>
2015-09-22 07:01:56 +0800

    add booth phontos into raffle draw page

--- routes/raffleDraw.js
+++ routes/raffleDraw.js
@@ -4,10 +4,13 @@
 var database = require('./database');
 
 exports.raffleDraw = function(req, res) {
-	database.getToasts('authorized', function(toasts) {
-		res.render('raffleDraw', {
-			title: 'Raffle Draw',
-			toasts: toasts,
-		});
-	});
+    database.getToasts('authorized', function(toasts) {
+        database.getBoothPhotos(function(photos) {
+            res.render('raffleDraw', {
+                title: 'Raffle Draw',
+                toasts: toasts,
+                photos: photos,
+            });
+        });
+    });
 };
